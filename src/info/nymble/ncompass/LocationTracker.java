dc75528e930472c53ae5699fdb7373e04793c878
proofsrock <proofsrock@2e6f1b3b-c840-0410-b4e6-a32ea3d982a7>
2008-01-14 02:55:39 +0000

    Added continuous compass update and migrated the view creation to XML

--- src/info/nymble/ncompass/LocationTracker.java
+++ src/info/nymble/ncompass/LocationTracker.java
@@ -2,12 +2,18 @@ package info.nymble.ncompass;
 
 import info.nymble.measure.Stopwatch;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentReceiver;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.util.Log;
 
 
 
@@ -16,21 +22,37 @@ import android.location.LocationProvider;
  * listing. If a provider becomes unavailable, will attempt 
  * to find another for the same data. 
  */
-public class LocationTracker
+public class LocationTracker 
 {
-    static final long ACCEPTABLE_AGE_THRESHOLD = 500;
+	private static final String LOCATION_CHANGED_INTENT = LocationTracker.class.getName();
+    private static final long ACCEPTABLE_AGE_THRESHOLD = 500;
     
-    LocationManager locationManager;
-    LocationProvider locationProvider;
+    private Intent intent = new Intent(LOCATION_CHANGED_INTENT);
+    private IntentFilter filter = new IntentFilter(LOCATION_CHANGED_INTENT);
+    private LocationUpdateReceiver intentReceiver = new LocationUpdateReceiver();
+    private ArrayList<LocationListener> listeners = new ArrayList<LocationListener>();
+    private boolean listening = false;
+    private boolean started = false;
     
-    Location currentLocation = new Location();
-    long lastCheckedTime = 0;
+    
+    private Context context;
+    private LocationManager locationManager;
+    private LocationProvider locationProvider;
+    
+    
+    private Location currentLocation = new Location();
+    private long lastCheckedTime = 0;
     
     
     
-    public LocationTracker(Context c)
+    
+    
+    
+    public LocationTracker(final Context c)
     {
         Stopwatch.start();
+        
+        context = c;
         locationManager = (LocationManager) c.getSystemService(Context.LOCATION_SERVICE);
         
         List<LocationProvider> list = locationManager.getProviders();
@@ -39,7 +61,7 @@ public class LocationTracker
         {
             locationProvider = list.get(i);
         }
-        
+
         Stopwatch.stop("find provider");
     }
     
@@ -60,6 +82,82 @@ public class LocationTracker
         
         return currentLocation;
     }
+
+
+    
+    
+    public void start()
+    {
+    	started = true;
+    	if (!listening && listeners.size() > 0)
+    	{	
+    		locationManager.requestUpdates(locationProvider, ACCEPTABLE_AGE_THRESHOLD, 0, intent);
+    		context.registerReceiver(intentReceiver, filter);
+    		listening = true;
+    	}
+    }
+    
+    public void stop()
+    {
+    	started = false;
+    	if (listening)
+    	{
+    		context.unregisterReceiver(intentReceiver);
+    	}
+    }
+    
+    public void registerLocationListener(LocationListener listener)
+    {
+    	listeners.add(listener);
+    	if (started && !listening) start();
+    }
+    
+    public void unregisterLocationListener(LocationListener listener)
+    {
+    	listeners.remove(listener);
+    	if (listening && listeners.size() <= 0) stop();
+    }
+    
+    
+    private void notifyObservers(Location l)
+    {
+		for (Iterator<LocationListener> i = listeners.iterator(); i.hasNext();) 
+		{
+			i.next().locationChanged(l);
+		}
+    }
+    
+    
+    public class LocationUpdateReceiver extends IntentReceiver 
+    {
+        @Override
+        public void onReceiveIntent(Context context, Intent intent) 
+        {
+        	updateLocation(intent); 
+        } 
+    }
+
+	void updateLocation(Intent intent) 
+	{
+		Location l = (Location)intent.getExtra("location");
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime - lastCheckedTime > ACCEPTABLE_AGE_THRESHOLD && locationProvider != null)
+        {
+        	Log.i(null, "Location update l=" + l.toString());
+            currentLocation = l;
+            lastCheckedTime = currentTime;
+            notifyObservers(l);
+        }
+	}
+    
+	
+	
+    
+    
+    
+    
+    
 
 //    private void logProvider(LocationProvider p)
 //    {
